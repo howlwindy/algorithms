87145dd819bce8ab5288c2462ef780df19e2636a
howlwindy <howlwindy@gmail.com>
2023-03-19 22:43:00 +0800

    linked list

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# algorithms
+# data structures
+
+- [ ] **linked list** [0](https://en.wikipedia.org/wiki/Linked_list)[1](https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/linked-list)
---
+++ data-structures/linked-list.ts
@@ -0,0 +1,18 @@
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  prepend() {}
+  append() {}
+  insert() {}
+  rm() {}
+  rmHead() {}
+  rmTail() {}
+  search() {}
+  sort() {}
+  reverse() {}
+  toString() {}
+  fromArray() {}
+  toArray() {}
+}
